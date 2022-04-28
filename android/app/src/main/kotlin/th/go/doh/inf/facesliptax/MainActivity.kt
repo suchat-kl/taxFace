6259f5366dfd09d9094987b998b692ee021951db
suchat.kl <suchat.kl@kkumail.com>
2022-04-28 12:51:44 +0700

    280465

---
+++ android/app/src/main/kotlin/th/go/doh/inf/facesliptax/MainActivity.kt
@@ -0,0 +1,12 @@
+package th.go.doh.inf.facesliptax
+
+import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+class MainActivity: FlutterActivity() {
+override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) 
+{GeneratedPluginRegistrant.registerWith(flutterEngine);}
+}
+
